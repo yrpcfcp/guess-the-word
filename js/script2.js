c1d2cd6d4e5c2b2487465f501e4b91d2ded6fa31
yrpcfcp <67036488+yrpcfcp@users.noreply.github.com>
2022-06-06 11:41:06 -0300

    .

--- js/script2.js
+++ js/script2.js
@@ -13,7 +13,7 @@ const guessedLetters = [];
 let remainingGuesses = 10;
 
 const getWord = async function () {
-  const response = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+  const response = await fetch("");
   const words = await response.text();
   const wordArray = words.split("\n");
   const randomIndex = Math.floor(Math.random() * wordArray.length);

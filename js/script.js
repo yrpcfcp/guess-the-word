b82f4a3e3595874d8f8e0d2adc53180c22d924f9
yrpcfcp <carlosfparana@gmail.com>
2022-06-20 22:08:19 -0300

    .

--- js/script.js
+++ js/script.js
@@ -23,13 +23,12 @@ const getWord = async function () {
 
 
 
-// Fire off the game
+
 getWord();
-// Display our symbols as placeholders for the chosen word's letters
+
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
-    // console.log(letter);
     placeholderLetters.push("●");
   }
   wordInProgress.innerText = placeholderLetters.join("");
@@ -37,16 +36,12 @@ const placeholder = function (word) {
 
 guessLetterButton.addEventListener("click", function (e) {
   e.preventDefault();
-  // Empty message paragraph
   message.innerText = "";
-  // Let's grab what was entered in the input
   const guess = letterInput.value;
-  // Let's make sure that it is a single letter
   const goodGuess = validateInput(guess);
 
   if (goodGuess) {
-    // We've got a letter! Let's guess!
-    makeGuess(guess);
+     makeGuess(guess);
   }
   letterInput.value = "";
 });
@@ -54,16 +49,16 @@ guessLetterButton.addEventListener("click", function (e) {
 const validateInput = function (input) {
   const acceptedLetter = /[a-zA-Z]/;
   if (input.length === 0) {
-    // Is the input empty?
+   
     message.innerText = "Please enter a letter.";
   } else if (input.length > 1) {
-    // Did you type more than one letter?
+    
     message.innerText = "Please enter a single letter.";
   } else if (!input.match(acceptedLetter)) {
-    // Did you type a number, a special character or some other non letter thing?
+    
     message.innerText = "Please enter a letter from A to Z.";
   } else {
-    // We finally got a single letter, omg yay
+    
     return input;
   }
 };
@@ -82,7 +77,7 @@ const makeGuess = function (guess) {
 };
 
 const showGuessedLetters = function () {
-  // Clear the list first
+
   guessedLettersElement.innerHTML = "";
   for (const letter of guessedLetters) {
     const li = document.createElement("li");
@@ -102,7 +97,6 @@ const updateWordInProgress = function (guessedLetters) {
       revealWord.push("●");
     }
   }
-  // console.log(revealWord);
   wordInProgress.innerText = revealWord.join("");
   checkIfWin();
 };
@@ -130,7 +124,6 @@ const updateGuessesRemaining = function (guess) {
   const upperWord = word.toUpperCase();
   
   if (!upperWord.includes(guess)) {
-    // womp womp - bad guess, lose a chance
     message.innerText = `Sorry, the word has no ${guess}.`;
     playSoundNo()
     remainingGuesses -= 1;
@@ -173,17 +166,15 @@ const startOver = function () {
 };
 
 playAgainButton.addEventListener("click", function () {
-  // reset all original values - grab new word
   message.classList.remove("win");
   guessedLetters = [];
   remainingGuesses = 8;
   remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
   guessedLettersElement.innerHTML = "";
   message.innerText = "";
-  // Grab a new word
   getWord();
 
-  // show the right UI elements
+ 
   guessLetterButton.classList.remove("hide");
   playAgainButton.classList.add("hide");
   remainingGuessesElement.classList.remove("hide");
